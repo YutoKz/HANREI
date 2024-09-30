2cd720292f82965a19b72c76e8469ac2074e3d5a
YuTo_Univ <yuuto140107k@gmail.com>
2024-09-30 22:56:29 +0900

    add: ask_llm

---
+++ src/hanrei/ask_llm.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def page_ask_llm():
+    st.title("Ask LLM")
+    st.write("This is the Ask LLM page.")
+    st.write("You can ask LLM anything here.")
---
+++ src/hanrei/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from ask_llm import page_ask_llm
+
+def init_page():
+    st.set_page_config(
+        page_title="HANREI",
+        page_icon="👩‍⚖️"
+    )
+
+def main():
+    init_page()
+
+    selection = st.sidebar.radio("Menu", ["Ask LLM"])
+    if selection == "Ask LLM":
+        page_ask_llm()
+
+
+
+if __name__ == '__main__':
+    main()
