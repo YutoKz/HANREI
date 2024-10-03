6e71132eb40c19301aaa3fc96e1e233d151d0ab4
YuTo_Univ <yuuto140107k@gmail.com>
2024-10-03 18:14:02 +0900

    ask_llm.py: ・LLMに質問＋法令検索をタブで1ページに ・類似文章をソートできるように

--- src/hanrei/ask_llm.py
+++ src/hanrei/ask_llm.py
@@ -11,7 +11,9 @@ from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 
+from datetime import datetime
 from prompts import prompts     # type: ignore
+
 
 QDRANT_PATH = "./local_qdrant"
 COLLECTION_NAME = "database"
@@ -71,6 +73,20 @@ def nengou(nengou_alphabet: str) -> str:
     else:
         return ""
 
+def nengou_to_year(nengou_alphabet: str, year: int) -> int:
+    if nengou_alphabet == "Reiwa":
+        return year + 2018
+    elif nengou_alphabet == "Heisei":
+        return year + 1988
+    elif nengou_alphabet == "Shouwa":
+        return year + 1925
+    elif nengou_alphabet == "Taishou":
+        return year + 1911
+    elif nengou_alphabet == "Meiji":
+        return year + 1867
+    else:
+        return year
+
 
 
 # -----------------------------------------------------------------------------------------------------
@@ -84,40 +100,73 @@ def page_ask_llm():
         qdrant = load_qdrant(COLLECTION_NAME)
     except Exception as e:
         st.error("ヒント：プロンプト入力時はクリックしたのち、処理が終わるまで待ってからAskボタンを押してください。")
-        st.error(f"---------------------------")
         st.error(f"Failed to load Qdrant: {e}")
         qdrant = None
 
-    col_query, col_askButton = st.columns((5, 1), vertical_alignment="bottom")
-    with col_query:
-        query = st.text_area("ChatGPT に質問：", key="Query", placeholder="あなたの置かれた状況について説明してください。")
-    with col_askButton:
-        ask_button = st.button("Ask", key="Ask") 
 
-    if qdrant and ask_button: # クエリが入力されたら
-        with st.spinner("ChatGPT is thinking..."):
-            retrieved_docs: list[Document] = qdrant.as_retriever(search_type="similarity", search_kwargs={"k":10}).invoke(query)
-            
-            qa_chain = (    # type: ignore
-                prompts("qa_chain")
-                | llm 
-                | StrOutputParser()        # 
-            )
-            output: str = qa_chain.invoke(  # type: ignore
-                {
-                    "context": format_docs(retrieved_docs),  # type: ignore
-                    "query": query,
-                }
-            )    # type: ignore
-        st.markdown("### 回答")
-        st.write(output) # type: ignore
+    tab_ask, tab_law = st.tabs(["Ask ChatGPT", "Search Law"])
 
-        # 類似文章を表示
-        st.markdown(f"### 関連する判例")
-        st.markdown("[関連度順]")
-        for i, doc in enumerate(retrieved_docs):
-            with st.expander(f"{nengou(doc.metadata.get("date_era"))}{doc.metadata.get("date_year")}年{doc.metadata.get("date_month")}月{doc.metadata.get("date_day")}日    {doc.metadata.get("court_name")}    **{doc.metadata.get("case_name")}**"):
-                st.markdown(f"事件番号：{doc.metadata.get('case_number')}")
-                st.markdown("..." + doc.page_content + "...")
-                st.markdown(f"裁判所HP 裁判例結果詳細：{doc.metadata.get('detail_page_link')}")
-                st.markdown(f"PDF Link：{doc.metadata.get('full_pdf_link')}")
+    with tab_ask:
+        col_query, col_askButton = st.columns((5, 1), vertical_alignment="bottom")
+        with col_query:
+            query = st.text_area("ChatGPT に質問：", key="Query", placeholder="あなたの置かれた状況について説明してください。")
+        with col_askButton:
+            ask_button = st.button("Ask", key="Ask") 
+
+        if qdrant and ask_button: # クエリが入力されたら
+            with st.spinner("Retrieving docs from VectorDB..."):
+                retrieved_docs: list[Document] = qdrant.as_retriever(search_type="similarity", search_kwargs={"k":10}).invoke(query)
+                
+            with st.spinner("ChatGPT is thinking..."):
+                qa_chain = (    # type: ignore
+                    prompts("qa_chain")
+                    | llm 
+                    | StrOutputParser()        # 
+                )
+                output: str = qa_chain.invoke(  # type: ignore
+                    {
+                        "context": format_docs(retrieved_docs),  # type: ignore
+                        "query": query,
+                    }
+                )    # type: ignore
+            st.session_state.retrieved_docs = retrieved_docs
+            st.session_state.output = output
+
+        if "output" in st.session_state:
+            st.markdown("### 回答")
+            st.write(st.session_state.output) # type: ignore
+
+        if "retrieved_docs" in st.session_state:
+            # 類似文章を表示
+            st.markdown(f"### 関連する判例")
+                #st.markdown("[関連度順]")
+            col_sort, col_reverse = st.columns((1, 1))
+            with col_sort:
+                selectbox_sort = st.selectbox("Sort by:", ["関連度順", "日付順"])   # type: ignore
+            with col_reverse:
+                selectbox_reverse = st.selectbox("Reverse:", ["降順", "昇順"])
+            if selectbox_sort == "関連度順":
+                for i, doc in enumerate(st.session_state.retrieved_docs):   # type: ignore
+                    with st.expander(f"{nengou(doc.metadata.get("date_era"))}{doc.metadata.get("date_year")}年{doc.metadata.get("date_month")}月{doc.metadata.get("date_day")}日    {doc.metadata.get("court_name")}    **{doc.metadata.get("case_name")}**"):     # type: ignore
+                        st.markdown(f"事件番号：{doc.metadata.get('case_number')}")  # type: ignore
+                        st.markdown("..." + doc.page_content + "...")
+                        st.markdown(f"裁判所HP 裁判例結果詳細：{doc.metadata.get('detail_page_link')}") # type: ignore
+                        st.markdown(f"PDF Link：{doc.metadata.get('full_pdf_link')}")   # type: ignore
+            elif selectbox_sort == "日付順":
+                list_date = []
+                for i, doc in enumerate(st.session_state.retrieved_docs):   # type: ignore
+                    list_date.append(datetime.strptime(f"{nengou_to_year(doc.metadata.get('date_era'), doc.metadata.get('date_year'))}-{doc.metadata.get('date_month')}-{doc.metadata.get('date_day')}", "%Y-%m-%d"))   # type: ignore
+                copied_docs = st.session_state.retrieved_docs.copy()
+                sorted_docs: list[Document] = [x for _, x in sorted(zip(list_date, copied_docs), key=lambda pair: pair[0], reverse=True if selectbox_reverse == "降順" else False)]  # type: ignore
+
+                for i, doc in enumerate(sorted_docs):   # type: ignore
+                    with st.expander(f"{nengou(doc.metadata.get("date_era"))}{doc.metadata.get("date_year")}年{doc.metadata.get("date_month")}月{doc.metadata.get("date_day")}日    {doc.metadata.get("court_name")}    **{doc.metadata.get("case_name")}**"):      # type: ignore
+                        st.markdown(f"事件番号：{doc.metadata.get('case_number')}")   # type: ignore
+                        st.markdown("..." + doc.page_content + "...")
+                        st.markdown(f"裁判所HP 裁判例結果詳細：{doc.metadata.get('detail_page_link')}")  # type: ignore
+                        st.markdown(f"PDF Link：{doc.metadata.get('full_pdf_link')}")   # type: ignore
+
+    with tab_law:
+        st.markdown("ここに法律検索のコードを書く")
+        st.markdown("データベースはSQLを検討中。。")
+        
