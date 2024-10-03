f5c8f85f56698ff1fa99c03315b3000c520a310c
YuTo_Univ <yuuto140107k@gmail.com>
2024-10-03 23:50:10 +0900

    ask_llm.py: answerで言及された法令を抽出、e-GOVからAPI経由で条文を取得・表示

--- src/hanrei/ask_llm.py
+++ src/hanrei/ask_llm.py
@@ -13,6 +13,9 @@ from qdrant_client.models import Distance, VectorParams
 
 from datetime import datetime
 from prompts import prompts     # type: ignore
+from law import get_num_from_name # type: ignore
+from law import get_num_from_name_keywords # type: ignore
+from law import get_law_from_num # type: ignore
 
 
 QDRANT_PATH = "./local_qdrant"
@@ -86,7 +89,11 @@ def nengou_to_year(nengou_alphabet: str, year: int) -> int:
         return year + 1867
     else:
         return year
-
+    
+def str_to_list(string: str) -> list[list[str]]:
+    lines = string.strip().split("\n")
+    result = [line.split(",") for line in lines]
+    return [[item.strip() for item in line] for line in result]
 
 
 # -----------------------------------------------------------------------------------------------------
@@ -169,7 +176,60 @@ def page_ask_llm():
                         st.markdown(f"PDF Link：{doc.metadata.get('full_pdf_link')}")   # type: ignore
 
     with tab_law:
-        st.markdown("ここに法律検索のコードを書く")
-        st.markdown("データベースはSQLを検討中。。")
-        # st.session_state.
+        # st.session_state.answer で言及されている法令を抽出、条文をAPIで取得して表示する
+        st.markdown("### 関連する法令")
+        if "answer" in st.session_state:
+            ## 法令抽出
+            extract_law_chain = (   # type: ignore
+                prompts("extract_law")
+                | llm
+                | StrOutputParser()
+                | str_to_list
+            )
+            extracted_laws: list[list[str]] = extract_law_chain.invoke({"context": st.session_state.answer})  # type: ignore
+            st.markdown(extracted_laws)      # これで、言及された法律の名称、条、項、号のリストが取得できた
+
+            ## 法令の検索
+            ## 言及された法令の名称が辞書内の法令に、　完全一致するなら条, 項, 号レベルで表示　/　部分一致なら名称のみでキーワード検索、結果を表示
+            for law in extracted_laws:   # law = [法令名, 条, 項, 号]
+                if len(law) != 4 or law[0] == "":  # type: ignore
+                    continue
+                
+                law_str: str = " ".join([law[0], law[1]])  # type: ignore
+                if law[1] != "":
+                    law_str += "条"
+                law_str += law[2]  # type: ignore
+                if law[2] != "":
+                    law_str += "項" # type: ignore
+                law_str += law[3] # type: ignore
+                if law[3] != "":
+                    law_str += "号" # type: ignore
+
+                st.markdown(f"##### {law_str}")
+
+                num_from_name = get_num_from_name(law[0])  # type: ignore
+                
+                ### 名称完全一致
+                if num_from_name != "": 
+                    # 法令番号から条文を取得
+                    with st.spinner("Retrieving law from API..."):
+                        with st.expander(law_str):      # type: ignore
+                            law_contents = get_law_from_num(num_from_name)  # type: ignore
+                            st.markdown(law_contents)   # type: ignore
+
+                ### 名称部分一致
+                else:
+                    # 法令名をキーワードとして、関連しそうな法令番号を取得
+                    num_from_name_keywords: dict[str, str] = get_num_from_name_keywords([law[0]])   # type: ignore
+
+                    # それぞれの条文を取得
+                    for i, (name, num) in enumerate(num_from_name_keywords.items()):    # type: ignore
+                        with st.spinner("Retrieving law from API..."):    
+                            if i >= 5:
+                                break
+                            with st.expander(f"{name}"):
+                                law_contents = get_law_from_num(num)    # type: ignore
+                                st.markdown(law_contents)   # type: ignore
+
+
         
